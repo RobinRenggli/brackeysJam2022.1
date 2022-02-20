533a821ca103007da57a2aa9da34d1f2531eb23c
SilvanRenggli <sili.renggli@gmail.com>
2022-02-20 18:45:20 +0100

    enemies move backwards

--- GodotProject/Enemies/Enemy.gd
+++ GodotProject/Enemies/Enemy.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 var position_history
 var playback_index
 var moving = false
+var move_backwards = false
 
 export (int) var speed = 200
 export (Texture) var face_right;
@@ -20,17 +21,36 @@ func _physics_process(delta):
 		var index = min(playback_index, position_history.size() - 1)
 		var history_entry = position_history[index]
 		print(history_entry)
-		playback_index += 1
 		global_position = history_entry["position"]
 		var direction = history_entry["direction"]
-		if direction.x > 0:
-			$Sprite.texture = face_right
-		elif direction.x < 0:
-			$Sprite.texture = face_left
-		elif direction.y > 0:
-			$Sprite.texture = face_down
-		elif direction.y < 0:
-			$Sprite.texture = face_up
+		if not move_backwards:
+			if direction.x > 0:
+				$Sprite.texture = face_right
+			elif direction.x < 0:
+				$Sprite.texture = face_left
+			elif direction.y > 0:
+				$Sprite.texture = face_down
+			elif direction.y < 0:
+				$Sprite.texture = face_up
+			if playback_index >= position_history.size() - 1:
+				playback_index -= 1
+				move_backwards = true
+			else:
+				playback_index += 1
+		else:
+			if direction.x > 0:
+				$Sprite.texture = face_left
+			elif direction.x < 0:
+				$Sprite.texture = face_right
+			elif direction.y > 0:
+				$Sprite.texture = face_up
+			elif direction.y < 0:
+				$Sprite.texture = face_down
+			if playback_index <= 0:
+				playback_index += 1
+				move_backwards = false
+			else:
+				playback_index -= 1
 
 func _on_Hitbox_body_entered(body):
 	EnemyStorage.stored_enemies = []
