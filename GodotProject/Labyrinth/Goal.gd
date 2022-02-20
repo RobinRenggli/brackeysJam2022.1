0645ddf5411a71c90e592a5a9e64cdeb123b278f
SilvanRenggli <sili.renggli@gmail.com>
2022-02-20 23:56:24 +0100

    spawn goal at random position

--- GodotProject/Labyrinth/Goal.gd
+++ GodotProject/Labyrinth/Goal.gd
@@ -2,4 +2,10 @@ extends Area2D
 
 func _on_Goal_body_entered(body):
 	body.on_goal_reached()
-	get_parent().remove_child(self)
+	respawn_at_random_position()
+
+func respawn_at_random_position():
+	# TODO: remove once goals get genareted when labyrinth grows
+	var rand = RandomNumberGenerator.new()
+	rand.randomize()
+	global_position = Vector2(200 + rand.randi_range(0, 5) * 400, 200 + rand.randi_range(0, 5) * 400)
