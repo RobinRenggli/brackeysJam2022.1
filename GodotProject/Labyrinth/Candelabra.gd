bc41060db09349c8b751668b56f664b10d533437
velvence <g.wiederkehr@outlook.com>
2022-02-22 21:59:38 +0100

    added Candlelight

--- GodotProject/Labyrinth/Candelabra.gd
+++ GodotProject/Labyrinth/Candelabra.gd
@@ -5,6 +5,14 @@ func _ready():
 
 func _on_Area2D_area_entered(area):
 	$Light2D.enabled = true
+	play_candle_on_animation()
 
 func _on_goal_reached():
 	$Light2D.enabled = false
+	play_candle_off_animation()
+
+func play_candle_on_animation():
+	$AnimationPlayer.play("candle_on")
+
+func play_candle_off_animation():
+	$AnimationPlayer.play("candle_off")
