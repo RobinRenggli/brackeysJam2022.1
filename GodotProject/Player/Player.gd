492cd94f0a0716479076f27a4c48aeeb0f5df64c
Alex <ak00mah@gmx.ch>
2022-02-24 00:07:08 +0100

    added fake fruit sounds

--- GodotProject/Player/Player.gd
+++ GodotProject/Player/Player.gd
@@ -62,7 +62,7 @@ func on_sanity_fruit_pickup():
 
 func on_fake_sanity_fruit_pickup():
 	sanity_counter_ui.set_sanity(sanity_counter_ui.get_sanity() - 1)
-	AudioController.get_node("SanityFruitSound").play()
+	AudioController.get_node("FakeFruitSound").play()
 
 func on_speed_fruit_pickup():
 	speed += 50
@@ -70,4 +70,4 @@ func on_speed_fruit_pickup():
 	
 func on_fake_speed_fruit_pickup():
 	speed = max(speed - 50, 50)
-	AudioController.get_node("SpeedFruitSound").play()
+	AudioController.get_node("FakeFruitSound").play()
