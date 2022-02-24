bb87afa74c0d651b3f9743984b290093940719df
Alex <ak00mah@gmx.ch>
2022-02-25 00:14:21 +0100

    for real this time

--- GodotProject/Player/SanityCounter.gd
+++ GodotProject/Player/SanityCounter.gd
@@ -26,8 +26,9 @@ func set_sanity(value):
 		EnemyStorage.stored_enemies = []
 		sanityUIFull.visible = false
 		sanityUIHalf.visible = false
-		yield(get_tree().create_timer(1), "timeout")
+		#yield(get_tree().create_timer(1), "timeout")
 		AudioController.get_node("DeathSound").play()
+		heartbeat()
 		TransitionScreen.transition()
 		yield(get_tree().create_timer(1), "timeout")
 		get_tree().change_scene("res://Scenes/GameOverScene.tscn")
@@ -53,8 +54,10 @@ func _on_goal_reached():
 	$Timer.set_wait_time(sanity_decrease_interval)
 
 func heartbeat():
-	yield(AudioController.get_node("Sync"), "timeout")
-	if sanity == 0:
+	if sanity > 0:
+		yield(AudioController.get_node("Sync"), "timeout")
+	print(sanity)
+	if sanity <= 0:
 		AudioController.get_node("Heartbeat1").stop()
 		AudioController.get_node("Heartbeat2").stop()
 		AudioController.get_node("Heartbeat3").stop()
